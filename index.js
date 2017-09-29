b1a738777f1f31c40826a7c1c1674a54871aab48
Bperez4197 <bryceperez31@gmail.com>
2017-09-29 07:43:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
-function sayHitToGrandma(string) {
+function sayHiToGrandma(string) {
+}
+if (sayHiToGrandma(string) === lowercase) {
   return "I can't hear you!"
-  If ((string) === lowercase)
 }
