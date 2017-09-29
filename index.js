3c15ea70cc85b2f59efe45ddeaedea4c58d2142d
Bperez4197 <bryceperez31@gmail.com>
2017-09-29 08:28:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function sayHiToGrandma(string) {
 if (string === whisper(string)) {
     return "I can't hear you!"
   }
-  if else (string === "I love you, Grandma")
+  else if (string === "I love you, Grandma")
     return "I love you, too."
   else (string === shout(string))
     return "YES INDEED!"
